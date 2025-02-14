d7772f98611b1e727895b5fc26f84341c7c6187b
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-14 23:02:49 +0530

    Update Nav.jsx
    update

--- E-DASHBOARD/frontend/src/components/Nav.jsx
+++ E-DASHBOARD/frontend/src/components/Nav.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import './Nav.css'
 
 const Nav = () => {
+  const auth = localStorage.getItem('user');
+  const navigate = useNavigate();
+  const logout = () =>{
+    localStorage.clear();
+    navigate('/signup');
+  }
+
   return (
     <div className='nav-first-div'>
       <ul className='nav-ul'>
         <li><Link to="/">Product</Link></li>
         <li><Link to="/add">Add product</Link></li>
         <li><Link to="/update">Update product</Link></li>
-        <li><Link to="/logout">Logout</Link></li>
+        {/* <li><Link to="/logout">Logout</Link></li> */}
         <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/signup">SignUp</Link></li>
+        <li>{auth ? <Link onClick={logout} to="/signup">Logout</Link> :
+        <Link to="/signup">SignUp</Link>}</li>
 
       </ul>
     </div>
