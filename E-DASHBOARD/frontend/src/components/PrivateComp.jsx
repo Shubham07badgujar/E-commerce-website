9c21d5491eb3417fa670f87041c0fad6c2314378
Shubham07badgujar <shubhambadgujari076@gmail.com>
2025-02-14 23:03:03 +0530

    Update PrivateComp.jsx
    private comp

--- E-DASHBOARD/frontend/src/components/PrivateComp.jsx
+++ E-DASHBOARD/frontend/src/components/PrivateComp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateComp = () => {
-    return <Outlet />
+    const auth = localStorage.getItem('user');
+    return auth? <Outlet />:<Navigate to="/signup" />;   
 };
 export default PrivateComp;
